9f6400ac50ad82b855313af8611cc4c0fc647824
Santiago ortiz <santyno09@gmail.com>
2016-11-22 17:02:10 -0300

    ovfv

---
+++ TrabajoPracticoNº3/EntidadesInstanciables_/Jornada.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones_;
+using Archivos;
+
+namespace EntidadesInstanciables_
+{
+    [Serializable]
+    public class Jornada
+    {
+        private List<Alumno> _alumnos;
+        private Gimnasio.EClases _clase;
+        private Instructor _instructor;
+
+
+        #region Constructores
+
+        /// <summary>
+        /// constructor por defecto que instancia la lista de alumnos.
+        /// </summary>
+        private Jornada()
+        {
+            this._alumnos = new List<Alumno>();
+        }
+
+        /// <summary>
+        /// instancia los atributos de la jornada, llamando al constructor por defecto.
+        /// </summary>
+        /// <param name="clase">clase</param>
+        /// <param name="instructor">instructor</param>
+        public Jornada(Gimnasio.EClases clase, Instructor instructor)
+            : this()
+        {
+            this._clase = clase;
+            this._instructor = instructor;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        /// <summary>
+        /// guarda la jornada den un archivo de texto.
+        /// </summary>
+        /// <param name="jornada">jornada</param>
+        /// <returns></returns>
+        public static bool Guardar(Jornada jornada)
+        {
+
+            Texto txt = new Texto();
+
+            txt.guardar("jornada.txt", jornada.ToString());
+
+            return true;
+        }
+
+        public static string Leer()
+        {
+            string aux;
+
+            Texto txt = new Texto();
+
+            txt.leer("jornada.txt", out aux);
+
+            return aux;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder nuevosb = new StringBuilder();
+
+            nuevosb.AppendLine("JORNADA");
+            nuevosb.Append("CLASE DE " + this._clase.ToString() + " POR " + this._instructor.ToString());
+            nuevosb.AppendLine("ALUMNOs:");
+
+            foreach (Alumno item in this._alumnos)
+            {
+                nuevosb.AppendLine(item.ToString());
+            }
+            nuevosb.AppendLine("<-------------------------------------------------->");
+
+            return nuevosb.ToString();
+        }
+
+        #endregion
+
+        #region Sobrecargas
+
+        /// <summary>
+        /// compara el alumno con la lista en jornada
+        /// </summary>
+        /// <param name="j">jornada</param>
+        /// <param name="a">alumno</param>
+        /// <returns>retorna true si el alumno esta em la lista</returns>
+        public static bool operator ==(Jornada j, Alumno a)
+        {
+            foreach (Alumno item in j._alumnos)
+            {
+                if (j._alumnos.Contains(a))
+                    return true;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// compara el alumno con la lista en jornada
+        /// </summary>
+        /// <param name="j">jornada</param>
+        /// <param name="a">alumno</param>
+        /// <returns>retorna true si el alumno NO esta em la lista</returns>
+        public static bool operator !=(Jornada j, Alumno a)
+        {
+            return !(j == a);
+        }
+
+        /// <summary>
+        /// agrega un alumno a la lista de la jornada si este no existe aun.
+        /// </summary>
+        /// <param name="j">jornada</param>
+        /// <param name="a">alumno</param>
+        /// <returns>retorna la jornada.</returns>
+        public static Jornada operator +(Jornada j, Alumno a)
+        {
+            foreach (Alumno item in j._alumnos)
+            {
+                if (item == a)
+                {
+                    throw new AlumnoRepetidoException();
+                }
+   
+            }
+            
+            j._alumnos.Add(a);
+            return j;
+        }
+
+        #endregion
+    }
+}
---
+++ TrabajoPracticoNº3/Excepciones_/SinInstructorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones_
+{
+    public class SinInstructorException : Exception
+    {
+        public SinInstructorException():base("No hay un instructor para la clase.")
+        {
+
+        }
+    }
+}
